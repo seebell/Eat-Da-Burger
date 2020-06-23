0045f45575f4e52969f0e3ec80621162b332bfc6
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-06-23 15:23:30 -0400

    wrote the code for orm

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,54 @@
 var connection = require("../config/connection");
 
-selectAll();
+function objToSql(ob) {
+    var arr = [];
 
-insertOne();
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = " ' " + value + " ' ";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
 
-updateOne();
+    return arr.toString();
+};
+
+var orm = {
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    insertOne: function (table, column, values, cb) {
+        var queryString = `INSERT INTO ${table} ( ${column.toString()} ) VALUES (?, ?)`;
+
+        console.log("Insert one : ");
+        console.log(queryString);
+
+        connection.query(queryString, function (err, results) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    updateOne: function (table, columnValues, condition, cb) {
+        var queryString = `UPDATE ${table} SET ${objToSql(columnValues)} WHERE ${condition}`;
+
+        console.log("UPDATE ONE");
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    }
+};
 
 
 module.exports = orm;
